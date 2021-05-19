a44d079e4e57ce306af72d9ef470df2501d428b4
Babatunde Kuku <gbolahantunde@gmail.com>
2021-05-19 05:28:50 +0100

    Added Chpt 3

--- src/com/bkuku/Bike.java
+++ src/com/bkuku/Bike.java
@@ -1,4 +1,33 @@
 package com.bkuku;
 
 public class Bike {
-}
+    private  String productName;
+    private boolean isOn;
+    private int speed;
+
+    public Bike(String productName) {
+        this.productName = productName;
+    }
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setOn(boolean isOn) {
+        this.isOn =isOn;
+    }
+    public boolean isOn() {
+        return isOn;
+    }
+    public void setAcceleration(boolean isOn) {
+        this.isOn =isOn;
+    }
+    public void setDeccelerate(boolean isOn) {
+        this.isOn =isOn;
+    }
+    public void Speed() {
+        if(isOn && speed>= 99);
+            speed = speed +5;
+    }
+
+    }
+
--- test/com/bkuku/BikeTest.java
+++ test/com/bkuku/BikeTest.java
@@ -2,16 +2,38 @@ package com.bkuku;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BikeTest {
+    Bike myBike = new Bike( "Honda");
 
-    @Test
-    public void testThatBikeCanBeTurnedOn(){
-        Bike okada = new Bike();
-
-//        okada.setOn(true);
-//        assertEquals(true, okada.isOn());
-        assertEquals(3, 2+2);
+         @Test
+        public void testThatBikeHasProductName(){
+        assertEquals("Honda" , myBike.getProductName());
     }
-}
+        @Test
+        public void testThatBikeCanBeTurnedOn() {
+        myBike.setOn(true);
+        myBike.setAcceleration(true);
+        myBike.setDeccelerate(true);
+        assertTrue(myBike.isOn());
+    }
+         @Test
+        public void testThatBikeCanBeTurnedOff(){
+        myBike.setOn(false);
+        assertFalse(myBike.isOn());
+    }
+        @Test
+         public void testForSpeedWhenAccelerating(){
+             myBike.setOn(true);
+             myBike.Speed();
+        }
+
+        }
+
+
+
+
+
+
+
