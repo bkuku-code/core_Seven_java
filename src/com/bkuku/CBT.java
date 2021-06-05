4fb6c7242f5e18fe86d42082753c869d332c7af7
Babatunde Kuku <gbolahantunde@gmail.com>
2021-06-05 02:32:41 +0100

    CBT

--- src/com/bkuku/CBT.java
+++ src/com/bkuku/CBT.java
@@ -11,13 +11,13 @@ public class CBT {
         int Number2 = (int) (Math.random()*10);
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("What is "+ Number1+ "+" + Number2+ "?" );
+        System.out.println("Solve "+ Number1+ "+" + Number2+ "?" );
         int answer = sc.nextInt();
 
         while(Number1 + Number2 != answer){
             System.out.println("Wrong answer . Try again. solve " + Number1 + "+" +Number2 + "?");
             answer = sc.nextInt();
         }
-        System.out.println("You Got it");
+        System.out.println("correct, Weldon");
     }
 }
